f8f9df53e212790c5b14224f159276b6127122df
Difimo <diegofigueiredomoreira@gmail.com>
2025-05-25 11:12:58 -0300

    Adicionado as outras variaveis e corrigos os erros de atribuição

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
+        // TODO: Conhecer e importar a classe scanner, exibir as mensagems para o nosso usuario
+        // obter pela classe scanner os valores digitados no terminal
+        // exibir a mensagem final ( msg da conta criada)
 
 public class ContaTerminal {
     
     public static void main(String[] args) throws Exception {
         Scanner entrada = new Scanner(System.in);
-        // TODO: Conhecer e importar a classe scanner, exibir as mensagems para o nosso usuario
-        // obter pela classe scanner os valores digitados no terminal
-        // exibir a mensagem final ( msg da conta criada)
+      
         Conta diegConta = new Conta();
 
 
@@ -16,13 +17,27 @@ public class ContaTerminal {
         String numero_agencia =  entrada.nextLine();
         diegConta.setAgencia(numero_agencia);
 
+        System.out.println("Por favor, digite o número da Conta!");
+        int numero_conta =  entrada.nextInt();
+        diegConta.setNumero(numero_conta);
+        // inserido um nextLine() vazio para corrigir o problema de buffer /n antes do nextLine() seguinte.
+        entrada.nextLine(); 
+
+
+        System.out.println("Por favor, digite o nome do titular da conta!");
+        String nome_conta =  entrada.nextLine();
+        diegConta.setNome_cliente(nome_conta);
+
+        System.out.println("Por favor, digite o Saldo do titular da conta!");
+        float saldo_conta =  entrada.nextFloat();
+        diegConta.setSaldo(saldo_conta);
 
 
 
-
-
-        System.out.println("Ola , nome !! obrigado por criar uma conta em nosso banco" +
-                            "sua agência é " + diegConta.getAgencia());
+        System.out.println(" Ola " +diegConta.getNome_cliente() +" !!!"+"\n Obrigado por criar uma conta em nosso banco " +
+                            "\n Sua agência é " + diegConta.getAgencia() + 
+                            "\n O numero da sua conta é " + diegConta.getNumero()+
+                            "\n O seu saldo em conta é R$ " + diegConta.getSaldo());
         
         
 
